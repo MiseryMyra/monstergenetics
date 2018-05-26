4a03614728e2a8c367754717783cbdcd558e321e
TheInventrix <TheInventrix@users.noreply.github.com>
2018-05-26 00:13:52 -0600

    fix crash on mouse-over plant
    stupid parentheses

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ def get_names_under_mouse():
         if obj.x == x and obj.y == y and (libtcod.map_is_in_fov(cfg.fov_map, obj.x, obj.y) or cfg.ALL_SEEING):
             name = obj.name
             
-            if obj.fighter and name is not 'plant':
+            if obj.fighter and (name is not 'plant'):
                 name = name.capitalize()
                 mon = obj.fighter
                 #stats = ' (HP:' + str(mon.hp) + '/' + str(mon.max_hp) + ' PW:' + str(mon.power) + ' DF:' + str(mon.defense) + ' DX:' + str(mon.dex) + ' SP:' + str(mon.speed) + ' PR:' + str(mon.perception) + ' LK:' + str(mon.luck) + ' SC:' + str(mon.social) + ' AG:' + str(mon.aggro) + ' XP:' + str(mon.xp) + ' NT:' + str(mon.nutrition) + '/' + str(mon.max_nutrition) + ')'
